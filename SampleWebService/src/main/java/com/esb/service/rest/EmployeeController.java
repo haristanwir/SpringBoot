65acc8b9b80b1fcb4ddf08ae7adf52cc22e13cc1
Haris Tanwir <Haris.Tanwir@ibm.com>
2021-06-08 00:50:24 +0500

    Update EmployeeController.java

--- SampleWebService/src/main/java/com/esb/service/rest/EmployeeController.java
+++ SampleWebService/src/main/java/com/esb/service/rest/EmployeeController.java
@@ -1,13 +1,18 @@
 package com.esb.service.rest;
 
+import java.util.List;
+import java.util.Map.Entry;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +24,14 @@ public class EmployeeController {
 	private EmployeeService employeeService;
 
 	@RequestMapping(path = { "/rest/employee/id", "/rest/employee/id/", "/rest/employee/id/{id}" }, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity getEmployeeById(@PathVariable(required = false) String id) {
-		return ResponseEntity.ok(employeeService.getEmployeeById(id));
+	public ResponseEntity getEmployeeById(@RequestHeader HttpHeaders headers, @PathVariable(required = false) String id) {
+		for (Object _header : headers.entrySet()) {
+			Entry<String, List<String>> header = (Entry<String, List<String>>) _header;
+			System.out.println(header.getKey() + " -> " + header.getValue());
+		}
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("Baeldung-Example-Header", "Value-ResponseEntityBuilderWithHttpHeaders");
+		return ResponseEntity.ok().headers(responseHeaders).body(employeeService.getEmployeeById(id));
 	}
 
 	@RequestMapping(path = "/rest/employee/name/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
